72a5f37b9b1a17b64b5badf0fe74bdd62960ea64
Ilya <53701582+IlyaShutov@users.noreply.github.com>
2020-02-21 20:48:58 +0300

    Add files via upload

---
+++ hw1.js
@@ -0,0 +1,45 @@
+'use strict'
+ cconst imageTraffic = document.querySelector('.drag');
+
+
+
+imageTraffic.onmousedown = (event) => {
+
+	imageTraffic.style.cursor = 'grab';
+	this.style.cursor = 'grab';
+	imageTraffic.style.position = 'absolute';
+  
+  imageTraffic.style.left = event.pageX - imageTraffic.offsetWidth / 2 + 'px';
+  imageTraffic.style.top = event.pageY - imageTraffic.offsetHeight / 2 + 'px';
+  
+  
+  
+  document.onmousemove = function(e) {
+    imageTraffic.style.left = e.pageX - imageTraffic.offsetWidth / 2 + 'px';
+    imageTraffic.style.top = e.pageY - imageTraffic.offsetHeight / 2 + 'px';
+  }
+
+  imageTraffic.onmouseup = function() {
+    document.onmousemove = null;
+    imageTraffic.onmouseup = null;
+    imageTraffic.style.cursor = 'pointer';
+    this.style.cursor = 'default';
+  }
+  function imageMove() {
+  
+  if (Mouse.Lbtn) {
+    Mouse.targetImage.style.top = Mouse.posY - Mouse.dy + "px";
+    Mouse.targetImage.style.left = Mouse.posX - Mouse.dx + "px";
+    window.requestAnimationFrame(imageMove);
+  }
+}
+
+
+<div class="drag"/>
+
+
+.drag {
+  height: 50px;
+  width: 50px;
+  border: 3px black solid;
+  cursor: pointer;
